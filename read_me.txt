c14af1e8a116951422f3784cbbe058ad6fe9ad24
aarjac <56480900+aarjac@users.noreply.github.com>
2025-12-15 18:35:24 -0500

    Implement player marks and game board display

---
+++ read_me.txt
@@ -0,0 +1,4 @@
+Tic-Tac-Toe
+Allows 2 human-players sitting at the same computer
+Move is selected 1-9
+Game board is printed after every move
--- tic_tac_toe_clean.py
+++ tic_tac_toe_clean.py
@@ -1,5 +1,69 @@
+def get_player_marks(player1_mark, player2_mark):
+    
+    player1_mark='WRONG'
+    acceptable_choices=['X','O']
+    
+    while player1_mark=='WRONG':
+
+        player1_mark=input('Player 1 enter your marker (X or O): ')
+        player1_mark=player1_mark.upper()
+
+        if player1_mark not in acceptable_choices:
+            
+            player1_mark='WRONG'
+            print('You did not enter X or O. Try again...')
+       
+        else:
+
+            acceptable_choices.remove(player1_mark)
+            player2_mark=acceptable_choices[0]
+
+    return player1_mark, player2_mark
+
+def print_board(game_board):
+
+    row_index=0
+    
+    for row in game_board.values():
+        
+        column_index=0
+        
+        for column in row:
+            
+            if column_index<(len(row)-1):
+                
+                print(column,' | ',end='')
+          
+            else:
+              
+                print(column)
+           
+            column_index+=1
+    
+        row_index+=1
+
+        if row_index<(len(row)):
+
+            print('-------------')
+        
+        else:
+            
+            print('\n')
+        
+
+
 def main():
-    pass
+   
+    player1_mark=''
+    player2_mark=''
+    game_board={'row1':[' ',' ',' '],'row2':[' ',' ',' '],'row3':[' ',' ',' ']}
+    
+    print('Welcome to the Tic-Tac-Toe Game\n')
+    player1_mark,player2_mark=get_player_marks(player1_mark, player2_mark)
+    print('\n')
+    
+    print_board(game_board)
+    
 
 if __name__=="__main__":
     main()
